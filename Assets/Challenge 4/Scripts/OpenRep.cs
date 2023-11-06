9a968eeacfde60dcfb1b2b2b4ce3eb93d84c537e
Matamenas <B00149694@mytudublin.ie>
2023-11-06 16:18:24 +0000

    Upload with button

---
+++ Assets/Challenge 4/Scripts/OpenRep.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenRep : MonoBehaviour
+{
+
+
+    public void OpenUrl()
+    {
+        Application.OpenURL("https://github.com/Matamenas/Challange4_B00149694");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
